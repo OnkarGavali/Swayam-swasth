6c16d3d7db3b8003e6ed6906866bde161f67c462
Aditya <sarnobatadi@gmail.com>
2022-11-28 10:03:01 +0530

    Email Alert Completed

--- Alert System/app.py
+++ Alert System/app.py
@@ -1,6 +1,39 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+import smtplib, ssl
+
+# smtp_server = "smtp.gmail.com"
+# port = 587  # For starttls
+# sender_email = "swayamswastha@gmail.com"
+# password = "Swayam@123"
+
+# # Create a secure SSL context
+# context = ssl.create_default_context()
+# server = smtplib.SMTP(smtp_server,port)
+# server.ehlo() # Can be omitted
+# server.starttls(context=context) # Secure the connection
+# server.ehlo() # Can be omitted
+# server.login(sender_email, password)
+# server.sendmail(sender_email, "sarnobatadi@gmail.com", "TEST")
+
+# smtp_server = "smtp.mail.yahoo.com"
+# port = 587  # For starttls
+sender_email = "swayamswastha@gmail.com"
+password = "usvvtpjbvkzcpgno"
+
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.ehlo()
+server.starttls() # Secure the connection
+server.ehlo()
+server.login(sender_email, password)
+subject = "Alert From SWAYAM SWASTHA !! "
+body = 'Heart Rate for your Patient was observed outside threshold contact Doctors !! '
+msg = f"Subject:{subject}\n\n{body}"
+
+
+
+
 
 cred = credentials.Certificate("serviceAccountKey.json")
 
@@ -10,6 +43,16 @@ firebase_admin.initialize_app(cred, {
 
 })
 
-ref = db.reference('/Patient')
+ref = db.reference('/devices')
 dict_patient = ref.get()
-print(dict_patient['BPM'])
+for n in dict_patient:
+    # print(n)
+    if(dict_patient[n]['email']!=""):
+        print(dict_patient[n]['email'])
+        emailid = dict_patient[n]['email']
+        server.sendmail(sender_email, emailid, msg)
+
+
+
+server.quit()
+
